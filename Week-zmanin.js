482c9d45ccca2e41d4ac5b7a308603b8755ad804
Chaim Gluck <Chaimshglick@gmail.com>
2024-04-04 13:32:00 -0400

    Put the hebcal program into git

---
+++ Week-zmanin.js
@@ -0,0 +1,22 @@
+import {HebrewCalendar, HDate, Location, Event, Zmanim} from '@hebcal/core';
+
+const location = Location.lookup("New York");
+const today = new HDate();
+const month = today.getMonth();
+let weekArray = [];
+let day = today.subtract(today.getDay()), i = 0;
+let d = day.getDay();
+
+// loop to push in the days of the week into the array
+while(d < 7){
+    weekArray.push({
+        weekday : day.add(i).getDay() +1,
+        "Hebrew Day" : `${day.add(i).getDate(month)}th of ${day.getMonthName(month)}, ${day.getFullYear()}`,
+        'Shma (Gr"a)' : new Zmanim(location, day.add(i)).sofZmanShma().toLocaleTimeString(),
+        'Shma (Mg"a)' : new Zmanim(location, day.add(i)).sofZmanShmaMGA().toLocaleTimeString(),
+        'Tfila (Gr"a)' : new Zmanim(location, day.add(i)).sofZmanTfilla().toLocaleTimeString(),
+        'Tfila (Mg"a)' : new Zmanim(location, day.add(i)).sofZmanTfillaMGA().toLocaleTimeString(),
+});
+    d++; i++;
+}
+console.table(weekArray)
